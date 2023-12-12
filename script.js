56ed35b729ef05f06c542d003d35b58e49707a0f
ryanp0126 <ryanpuahzhiyuan@gmail.com>
2023-12-11 23:36:36 -0800

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function displayPositive() {
+    document.getElementById("message").textContent = "Thanks for the good feedback!";
+}
+
+function displayNegative() {
+    document.getElementById("message").textContent = "Sorry for your experience! We'll try to do better!"
+}
